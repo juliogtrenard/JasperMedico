f50e4d571d3bd87a79f46d2df74c05c581360716
Julio González <juliogtrenard@gmail.com>
2025-01-07 20:16:46 +0100

    ADD diseño fxml

--- src/main/java/es/juliogtrenard/jaspermedico/MedicoApplication.java
+++ src/main/java/es/juliogtrenard/jaspermedico/MedicoApplication.java
@@ -7,16 +7,23 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Clase que muestra un informe médico a partir de un formulario
+ */
 public class MedicoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MedicoApplication.class.getResource("/es/juliogtrenard/jaspermedico/fxml/medico.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("FORMULARIO MÉDICO");
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * El punto de entrada de la aplicación
+     * @param args argumentos de linea de comandos
+     */
     public static void main(String[] args) {
         launch();
     }
